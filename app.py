a86a38796df5505d3508cb7a2bf35749e1be639f
Yogesh J. Baiskar <68940642+YJB28@users.noreply.github.com>
2023-05-30 16:39:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,28 @@ from flask import Flask, render_template, request, url_for, flash, jsonify
 from werkzeug.utils import redirect
 from flask_mysqldb import MySQL
 import mysql.connector
-
+import os
 
 app = Flask(__name__)
+
+# Get MySQL database configuration from environment variables
+db_host = os.environ.get('DB_HOST')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
+
+# Connect to MySQL database
 mydb = mysql.connector.connect(
-    host="mysqldb-service",
-    user="root",
-    password="root",
-    database="pmt",
-  
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
 )
+
 cursor = mydb.cursor()
 
-
 mysql = MySQL(app)
+
 @app.route('/login', methods=['POST'])
 def pm_login():
     try:
